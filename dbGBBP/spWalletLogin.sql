501a24010ab146852d544aabab43430092ea0415
Mark Waser <mark.waser@wisdom.digital>
2020-05-12 13:41:12 -0400

    Added Wallet Login

---
+++ dbGBBP/spWalletLogin.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spWalletLogin]
+	@pAddress varchar(50)
+AS
+	DECLARE @id int
+
+	SELECT @id = MemberId FROM MemberAddrs WHERE Address = @pAddress
+	UPDATE Members SET LastLogin = SYSDATETIME() WHERE Id = @id
+	SELECT * FROM Members WHERE Id = @id FOR JSON PATH
+
+RETURN 0
