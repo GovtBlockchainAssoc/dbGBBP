ccce0fcc5bab3ba52641d8329f7cc4a6daf54701
Mark Waser <mark.waser@wisdom.digital>
2020-05-16 15:29:09 -0400

    5/16 Update

--- dbGBBP/spMemberUpdate.sql
+++ dbGBBP/spMemberUpdate.sql
@@ -4,12 +4,14 @@
 	@pEmail		varchar(50), 
 	@pStatusId	int, 
 	@pStatusStr	varchar(20),
-	@pPoAAddr	varchar(50)
+	@pBChain	int,
+	@pAddress	varchar(50)
 AS
 
 	UPDATE Members SET Name = @pName, Email = @pEmail, StatusId = @pStatusId, StatusStr = @pStatusStr WHERE Id = @pId
-	IF (@pPoAAddr != '' AND NOT(EXISTS(SELECT 1 FROM MemberAddrs WHERE MemberId = @pId AND Address = @pPoAAddr)))
-		INSERT INTO MemberAddrs (MemberId, BlockchainId, Address) VALUES (@pId, 1, @pPoAAddr)
+	IF (ISNULL(@pAddress, '') != '' AND ISNULL(@pBChain, 0) != 0 AND
+		NOT(EXISTS(SELECT 1 FROM MemberAddrs WHERE MemberId = @pId AND BlockchainId = @pBChain AND Address = @pAddress)))
+			INSERT INTO MemberAddrs (MemberId, BlockchainId, Address) VALUES (@pId, @pBChain, @pAddress)
 
 	SELECT * FROM Members WHERE Id = @pId FOR JSON PATH
 
