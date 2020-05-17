ad7ffe6862a0529d050ca6862a7cc0b30ecafc2e
Mark Waser <mark.waser@wisdom.digital>
2020-05-17 15:11:08 -0400

    Wallet Upgrade

--- dbGBBP/spTokenTxs.sql
+++ dbGBBP/spTokenTxs.sql
@@ -6,12 +6,15 @@ DECLARE @Txs TABLE (Id int IDENTITY(1,1), TxId int, My_Chain varchar(20), My_Add
 	Their_Name varchar(50), Their_Chain varchar(20), Their_Addr varchar(50), Addr varchar(50), Date_Time varchar(50), Updated datetime2, Balance decimal(28, 12))
 
 INSERT INTO @Txs (TxId, My_Chain, My_Addr, Token, Amount, Memo, Their_Chain, Their_Addr, Addr, Date_Time, Updated) 
-	(SELECT tx.Id, FromChain, '...' + RIGHT(FromAddr, 6), Token, -1 * Value, Memo, DestChain, '...' + RIGHT(DestAddr, 6), DestAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), Updated 
+	(SELECT tx.Id, FromChain, FromAddr, Token, -1 * Value, Memo, DestChain, DestAddr, DestAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), Updated 
 	FROM TokenTxs tx INNER JOIN MemberAddrs a ON tx.FromAddr = a.Address WHERE MemberId = @pMemberId
 	UNION
-	SELECT tx.Id, DestChain, '...' + RIGHT(DestAddr, 6), Token, Value, Memo, FromChain, '...' + RIGHT(FromAddr, 6), FromAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), Updated 
+	SELECT tx.Id, DestChain, DestAddr, Token, Value, Memo, FromChain, FromAddr, FromAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), Updated 
 	FROM TokenTxs tx INNER JOIN MemberAddrs a ON tx.ToAddr = a.Address WHERE MemberId = @pMemberId)
 	ORDER BY Updated
+
+UPDATE @Txs SET My_Chain = 'GBA Hub' WHERE My_Chain = 'Core PoA'
+UPDATE @Txs SET Their_Chain = 'GBA Hub' WHERE Their_Chain = 'Core PoA'
 
 UPDATE @Txs SET Amount = Amount/(POWER(10, Decimals)) FROM @Txs tx INNER JOIN GBATokens t ON t.Symbol = tx.Token
 UPDATE @Txs SET Their_Name = m.[Name] FROM @Txs tx INNER JOIN MemberAddrs ma ON ma.Address = tx.Addr INNER JOIN Members m ON m.Id = ma.MemberId
@@ -19,8 +22,8 @@ UPDATE @Txs SET Their_Name = m.[Name] FROM @Txs tx INNER JOIN MemberAddrs ma ON 
 DECLARE @Tokens TABLE (PocketId int identity(1,1), Chain varchar(20), Addr varchar(50), Token varchar(10), Balance decimal(28, 12), Actions varchar(100))
 DECLARE @TokenTxs TABLE (Id int identity(1,1), PocketId int, TxId int, Amount decimal(28, 12), Balance decimal(28, 12))
 
-INSERT INTO @Tokens (Token, Chain, Addr, Actions) 
-	SELECT DISTINCT Token, My_Chain, My_Addr, '{<SendDialog tokenAcct="' + Token + '|' + My_Chain + '|' + My_Addr + '" />}' FROM @Txs
+INSERT INTO @Tokens (Token, Chain, Addr) 
+	SELECT DISTINCT Token, My_Chain, My_Addr FROM @Txs
 INSERT INTO @TokenTxs (PocketId, TxId, Amount) SELECT PocketId, TxId, Amount
 	FROM @Txs x INNER JOIN @Tokens t ON x.My_Chain = t.Chain AND x.My_Addr = t.Addr AND x.Token = t.Token
 	ORDER BY PocketId, Updated
