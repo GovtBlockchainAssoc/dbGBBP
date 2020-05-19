4811f3c33725aa149c7ca3529cf6c7389c3a2864
Mark Waser <mark.waser@wisdom.digital>
2020-05-19 14:50:16 -0400

    Hive send

--- dbGBBP/spTokenEvent.sql
+++ dbGBBP/spTokenEvent.sql
@@ -1,23 +1,26 @@
 ﻿CREATE PROCEDURE [dbo].[spTokenEvent]
-	@pTxHash varchar(50), @pEvent varchar(20), @pToken VARCHAR(10) = '', @pFromChain VARCHAR(50) = '', @pFromBlock INT = 0, @pFromAddr VARCHAR(50) = '', 
+	@pTxHash varchar(20), @pEvent varchar(20), @pToken VARCHAR(10) = '', @pFromChain VARCHAR(20) = '', @pFromBlock varchar(20) = '', @pFromAddr VARCHAR(50) = '', 
     @pToAddr VARCHAR(50) = '', @pValue DECIMAL(28,12) = 0, @pMemo VARCHAR(50) = '', 
-    @pClaimBy VARCHAR(50) = '', @pClaimBlock INT = 0, @pFee DECIMAL(28,12) = 0,
-    @pDestChain VARCHAR(50) = '', @pDestBlock VARCHAR(50) = '', @pDestAddr VARCHAR(50) = ''
+    @pClaimBy VARCHAR(50) = '', @pClaimBlock varchar(20) = '', @pFee DECIMAL(28,12) = 0,
+    @pDestChain VARCHAR(20) = '', @pDestBlock VARCHAR(20) = '', @pDestAddr VARCHAR(50) = ''
 AS
 	-- A MemoTransfer will always generate a Transfer but either can come first and we don't want duplicates
 
 	IF @pEvent = 'Transfer' BEGIN
-		IF EXISTS(SELECT 1 FROM TokenEvents WHERE TxHash = @pTxHash AND FromAddr = @pFromAddr AND ToAddr = @pToAddr) RETURN 0
+		IF EXISTS(SELECT 1 FROM TokenEvents WHERE TxHash = @pTxHash AND FromAddr = @pFromAddr AND ToAddr = @pToAddr) BEGIN
+			SELECT * FROM TokenEvents WHERE TxHash = @pTxHash AND FromAddr = @pFromAddr AND ToAddr = @pToAddr FOR JSON PATH
+			RETURN 0
+		END
 
 		INSERT INTO TokenEvents (TxHash, [Block], Operation, Token, FromChain, FromBlock, FromAddr, ToAddr, Value, Memo, DestChain, DestBlock, DestAddr) 
 			VALUES (@pTxHash, @pFromBlock, @pEvent, @pToken, @pFromChain, @pFromBlock, @pFromAddr, @pToAddr, @pValue, @pMemo, @pDestChain, @pDestBlock, @pDestAddr)
 
-		IF (@pDestChain = '') SELECT @pDestChain = @pFromChain, @pDestBlock = @pFromBlock, @pDestAddr = @pToAddr
+		SELECT @pDestChain = @pFromChain, @pDestBlock = @pFromBlock, @pDestAddr = @pToAddr
 		INSERT INTO TokenTxs (TxHash, Token, FromChain, FromBlock, FromAddr, ToAddr, Value, Memo, DestChain, DestBlock, DestAddr) 
 			VALUES (@pTxHash, @pToken, @pFromChain, @pFromBlock, @pFromAddr, @pToAddr, @pValue, @pMemo, @pDestChain, @pDestBlock, @pDestAddr)
 	END
 
-	IF @pEvent = 'MemoTransfer' BEGIN
+	IF (@pEvent = 'MemoTransfer' OR @pEvent = 'CBTransfer') BEGIN
 		IF (@pDestChain = '') SELECT @pDestChain = @pFromChain, @pDestBlock = @pFromBlock, @pDestAddr = @pToAddr
 		IF EXISTS(SELECT 1 FROM TokenEvents WHERE TxHash = @pTxHash AND FromAddr = @pFromAddr AND ToAddr = @pToAddr) BEGIN
 			UPDATE TokenEvents SET Operation = @pEvent, Memo = @pMemo, DestChain = @pDestChain, DestBlock = @pDestBlock, DestAddr = @pDestAddr 
@@ -31,6 +34,8 @@ AS
 				VALUES (@pTxHash, @pToken, @pFromChain, @pFromBlock, @pFromAddr, @pToAddr, @pValue, @pMemo, @pDestChain, @pDestBlock, @pDestAddr)
 		END
 	END
+
+	SELECT * FROM TokenEvents WHERE TxHash = @pTxHash AND FromAddr = @pFromAddr AND ToAddr = @pToAddr FOR JSON PATH
 
 
 
--- dbGBBP/spTokenTxs.sql
+++ dbGBBP/spTokenTxs.sql
@@ -3,18 +3,15 @@
 AS
 
 DECLARE @Txs TABLE (Id int IDENTITY(1,1), TxId int, My_Chain varchar(20), My_Addr varchar(50), Token varchar(10), Amount decimal(28, 12), Memo varchar(MAX), 
-	Their_Name varchar(50), Their_Chain varchar(20), Their_Addr varchar(50), Addr varchar(50), Date_Time varchar(50), Updated datetime2, Balance decimal(28, 12))
+	Their_Name varchar(50), Their_Chain varchar(20), Their_Addr varchar(50), Addr varchar(50), Date_Time varchar(50), Updated datetime2, FromBlock varchar(20), Balance decimal(28, 12))
 
-INSERT INTO @Txs (TxId, My_Chain, My_Addr, Token, Amount, Memo, Their_Chain, Their_Addr, Addr, Date_Time, Updated) 
-	(SELECT tx.Id, FromChain, FromAddr, Token, -1 * Value, Memo, DestChain, DestAddr, DestAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), Updated 
+INSERT INTO @Txs (TxId, My_Chain, My_Addr, Token, Amount, Memo, Their_Chain, Their_Addr, Addr, Date_Time, Updated, FromBlock) 
+	(SELECT tx.Id, FromChain, FromAddr, Token, -1 * Value, Memo, DestChain, DestAddr, DestAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), DATEADD(minute, DATEDIFF(minute, 0, Updated), 0) AS Updated, FromBlock 
 	FROM TokenTxs tx INNER JOIN MemberAddrs a ON tx.FromAddr = a.Address WHERE MemberId = @pMemberId
 	UNION
-	SELECT tx.Id, DestChain, DestAddr, Token, Value, Memo, FromChain, FromAddr, FromAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), Updated 
-	FROM TokenTxs tx INNER JOIN MemberAddrs a ON tx.ToAddr = a.Address WHERE MemberId = @pMemberId)
-	ORDER BY Updated
-
-UPDATE @Txs SET My_Chain = 'GBA Hub' WHERE My_Chain = 'Core PoA'
-UPDATE @Txs SET Their_Chain = 'GBA Hub' WHERE Their_Chain = 'Core PoA'
+	SELECT tx.Id, DestChain, DestAddr, Token, Value, Memo, FromChain, FromAddr, FromAddr, FORMAT(Updated, 'M/d/yy h:mm:ss tt'), DATEADD(minute, DATEDIFF(minute, 0, Updated), 0) AS Updated, FromBlock 
+	FROM TokenTxs tx INNER JOIN MemberAddrs a ON tx.DestAddr = a.Address WHERE MemberId = @pMemberId)
+	ORDER BY Updated, FromBlock
 
 UPDATE @Txs SET Amount = Amount/(POWER(10, Decimals)) FROM @Txs tx INNER JOIN GBATokens t ON t.Symbol = tx.Token
 UPDATE @Txs SET Their_Name = m.[Name] FROM @Txs tx INNER JOIN MemberAddrs ma ON ma.Address = tx.Addr INNER JOIN Members m ON m.Id = ma.MemberId
@@ -24,9 +21,9 @@ DECLARE @TokenTxs TABLE (Id int identity(1,1), PocketId int, TxId int, Amount de
 
 INSERT INTO @Tokens (Token, Chain, Addr) 
 	SELECT DISTINCT Token, My_Chain, My_Addr FROM @Txs
-INSERT INTO @TokenTxs (PocketId, TxId, Amount) SELECT PocketId, TxId, Amount
+INSERT INTO @TokenTxs (PocketId, TxId, Amount) SELECT PocketId, Id, Amount
 	FROM @Txs x INNER JOIN @Tokens t ON x.My_Chain = t.Chain AND x.My_Addr = t.Addr AND x.Token = t.Token
-	ORDER BY PocketId, Updated
+	ORDER BY PocketId, Updated, FromBlock
 
 DECLARE @id int, @pocketId int, @amount decimal(28, 12), @oldPocketId int, @balance decimal(28, 12)
 SET @oldPocketId = 0
@@ -41,10 +38,10 @@ WHILE @@FETCH_STATUS = 0 BEGIN
 	FETCH NEXT FROM cursor_txs INTO @id,  @pocketId, @amount
 END;
 CLOSE cursor_txs
-UPDATE @Txs SET Balance = tt.Balance FROM @Txs t INNER JOIN @TokenTxs tt ON tt.Id = t.Id
+UPDATE @Txs SET Balance = tt.Balance FROM @Txs t INNER JOIN @TokenTxs tt ON tt.TxId = t.Id
 
 -- SELECT * FROM @Tokens
 -- SELECT * FROM @Txs ORDER BY DTS Desc, Id Desc
-SELECT (SELECT * FROM @Tokens FOR JSON PATH) AS table1, (SELECT * FROM @Txs ORDER BY Updated Desc, Id Desc FOR JSON PATH) AS table2 FOR JSON PATH
+SELECT (SELECT * FROM @Tokens FOR JSON PATH) AS table1, (SELECT * FROM @Txs ORDER BY Updated Desc, FromBlock Desc, Id Desc FOR JSON PATH) AS table2 FOR JSON PATH
 
 RETURN 0
